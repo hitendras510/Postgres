f3ee583475df443932f45603288b52365d8f3772
Hitendra singh <hitendrasingh5489@gmail.com>
2026-01-18 23:35:55 +0530

    . 

--- src/index.ts
+++ src/index.ts
@@ -1,21 +1,32 @@
 import { Client } from "pg";
+import e, { express } from "express";  
+import type{Request,Response} from "express";
+
+const app = express();
+app.use(express.json());
 
 const pgClient = new Client({
   connectionString:
     "postgresql://neondb_owner:npg_xoLhCbnf24FU@ep-late-union-ahbwqvj6-pooler.c-3.us-east-1.aws.neon.tech/neondb?sslmode=require&channel_binding=require",
 });
 
-async function main() {
-  try {
-    await pgClient.connect();
+app.post("signup", async (req: Request, res:Response) => {
+   const { username, email, password } = req.body; 
+   try {
+    const query = `INSERT INTO users (username,email,password) VALUES ($1 ,$2, $3);`
+    const response = await pgClient.query(query,[username,email,password]);
+       res.json({
+        message: "You have signed up"
+       })
+    
+   } catch (e) {
+    console.log(e);
+    res.json({
 
-    const response = await pgClient.query("");
-    console.log(response.rows);
-  } catch (error) {
-    console.error("Database error:", error);
-  } finally {
-    await pgClient.end();
-  }
-}
+    })
+   }
+});
 
-main();
+app.port(3000);
+// const insertQuery = `INSERT INTO  todo (username, email,password) VALUES ('${username}','${email}','${password}')`;
+// const response = await pgClient.query(insertQuery); 
