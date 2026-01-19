7784e35920e439f597c467dc84ede1f90e160781
Hitendra singh <hitendrasingh5489@gmail.com>
2026-01-19 13:56:34 +0530

    . 

--- src/index.ts
+++ src/index.ts
@@ -7,7 +7,7 @@ app.use(express.json());
 
 const pgClient = new Client({
   connectionString:
-    "postgresql://neondb_owner:npg_xoLhCbnf24FU@ep-late-union-ahbwqvj6-pooler.c-3.us-east-1.aws.neon.tech/neondb?sslmode=require&channel_binding=require",
+    "",
 });
 pgClient.connect();
 
